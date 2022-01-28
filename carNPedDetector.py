f6ba13d4f81ccbd66250561fedd8ac9b2c620fad
rickyjena <ricky.jena@gmail.com>
2022-01-28 14:21:15 -0800

    Created data vars and loaded in datasets

--- carNPedDetector.py
+++ carNPedDetector.py
@@ -1,4 +1,17 @@
 import cv2
 
+trainPedFileName = 'haarcascade_fullbody.xml'
+
+trainCarFileName = 'carsTrainingData.xml'
+
+videoFileName = 'jdm.mp4'
+
+# This is the pretrained data for pedestrians loaded in with opencv
+trainedPedData = cv2.CascadeClassifier(trainPedFileName)
+
+# This pretrained car data being loaded in using opencv
+trainedCarData = cv2.CascadeClassifier(trainCarFileName)
+
+vehicleCamData = videoFileName
 
 print("Detected cars and pedestrians succesfully....")
