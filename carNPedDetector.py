0a9754ff85495bb77ea2b103310ac60770c84423
rickyjena <ricky.jena@gmail.com>
2022-01-28 18:20:18 -0800

    Now able to to view test vid with opencv

--- carNPedDetector.py
+++ carNPedDetector.py
@@ -12,6 +12,32 @@ trainedPedData = cv2.CascadeClassifier(trainPedFileName)
 # This pretrained car data being loaded in using opencv
 trainedCarData = cv2.CascadeClassifier(trainCarFileName)
 
-vehicleCamData = videoFileName
+# Loaded in video file for to act as cehicle camera
+vehicleCamData = cv2.VideoCapture(videoFileName)
 
-print("Detected cars and pedestrians succesfully....")
+# Looping through frames for camera
+while True:
+
+    # Take in current frame
+    readSuccess, captFrame = vehicleCamData.read()
+
+    # Checks if video input worked
+    if readSuccess:
+        "Nice Ready for GrayScale"
+    else:
+        break
+
+    # Shows video
+    cv2.imshow('Car and Pedestrian Detector', captFrame)
+
+    # Time to wait for each frame in video
+    key = cv2.waitKey(1)
+
+    # Can end program by pressing b
+    if key == 98:
+        break
+
+# Clears mem
+vehicleCamData.release()
+
+print("Program Compiled")
